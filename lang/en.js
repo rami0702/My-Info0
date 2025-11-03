3b4cee42bbc6eb4b94174dd4b0c2f28d90cb31b9
rami0702 <maher99alftaeh@gmail.com>
2025-11-03 02:31:08 +0100

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -82,7 +82,7 @@ Object.assign(en, {
     scriptVersion: 'PowerShell Edition v4.0',
     dashboardTitle: 'Analytics Dashboard',
     dashboardDesc: 'An interactive dashboard for real-time data analysis and visualization.',
-    trademarkText: 'RAM™ - All rights reserved | Developed by the RAM team',
+    trademarkText: 'RAM™ - All rights reserved | Developed by the RAM',
     trademarkText_line1: 'RAM™ - All rights reserved',
     trademarkText_line2: 'Developed by the RAM'
 });
